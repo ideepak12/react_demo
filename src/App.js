8c5817ebeecb1670a537342ab0fb91fb9c76fc23
ideepak12 <ideepakyadav12@gmail.com>
2018-07-02 15:52:18 +0530

    Change state.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,23 @@ import Person from './Person/Person';
 
 class App extends Component {
   state = {
-    person: [
-      { name: "deepak" },
-      { name: "min" },
-      { name: "max" }
-    ]
+    persons: [
+      { name: "deepak", age: "26" },
+      { name: "min", age: "16" },
+      { name: "max", age: "20" }
+    ],
+    otherState: "Some other state"
+  }
+
+  switchNameHandler = () => {
+    // Don't do this, this.state.persons[0].name = "yadav";
+    this.setState( {
+      persons: [
+        { name: "yadav", age: "26" },
+        { name: "min", age: "16" },
+        { name: "max", age: "100" }
+      ]
+    } )
   }
 
   render() {
@@ -16,11 +28,11 @@ class App extends Component {
       <div className="App">
         <h1> Hello World </h1>
 
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
 
-        <Person name={this.state.person[0].name} />
-        <Person name={this.state.person[1].name} > My hobbie: Racing </Person>
-        <Person name={this.state.person[2].name} />
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My hobbie: Racing </Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     // OR
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const person = (props) => {
 	return (
 		<div>
-			<p> This is {props.name}, and I am {Math.floor(Math.random() * 25)} years old. </p>
+			<p> This is {props.name}, and I am {props.age} years old. </p>
 			{/* To show opening & closing tag content. */}
 			<p> {props.children} </p>
 		</div>
