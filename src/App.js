73cbf5c9576af1e48d1cf158e54d1d43c44eb62e
ideepak12 <ideepakyadav12@gmail.com>
2018-07-02 15:29:54 +0530

    Use of state.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    person: [
+      { name: "deepak" },
+      { name: "min" },
+      { name: "max" }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1> Hello World </h1>
 
-        <Person name="deepak" />
-        <Person name="max"> My hobbie: Racing </Person>
-        <Person name="min" />
+        <button>Switch Name</button>
+
+        <Person name={this.state.person[0].name} />
+        <Person name={this.state.person[1].name} > My hobbie: Racing </Person>
+        <Person name={this.state.person[2].name} />
       </div>
     );
     // OR
