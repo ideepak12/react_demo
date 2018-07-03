9586fa50aa43f7a347036facb7630fc3b7b595c5
ideepak12 <ideepakyadav12@gmail.com>
2018-07-03 15:46:53 +0530

    Show repeated components using 'map' funcion.

--- src/App.js
+++ src/App.js
@@ -55,21 +55,11 @@ class App extends Component {
     if ( this.state.showPersons ) {
       person = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-            click={() => this.switchNameHandler("xyz")}
-          />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, "abc")}
-            change={this.nameChangedHandler}
-          > My hobbie: Racing </Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}
-          />
+          {this.state.persons.map(person => {
+            return <Person
+              name={person.name}
+              age={person.age} />
+          })}
         </div>
       )
     }
